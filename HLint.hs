8a2f521a9290ebb316d7680d252df6bdaa4fd3b9
Roman Gonzalez <romanandreg@gmail.com>
2017-08-26 05:02:44 +0000

    Remove HLint.hs in favor of .hlint.yml

--- HLint.hs
+++
@@ -1,9 +0,0 @@
--- HLint configuration file
-
-module HLint.HLint where
-
-import "hint" HLint.Default
-import "hint" HLint.Builtin.All
-
--- Protolude removes the String alias to avoid its usage
-ignore "Use String"
