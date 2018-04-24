44a772aa397a157a3f726fe38f25b6ba3686b719
Roman Gonzalez <open-source@roman-gonzalez.info>
2018-04-24 14:20:30 +0000

    Replace criterion in favor of gauge

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,29 +5,29 @@ module Main where
 
 import RIO
 
-#if MIN_VERSION_gauge(1,3,0)
+#if MIN_VERSION_gauge(0,2,0)
+import Gauge
+#else
+import Gauge
 import Gauge.Main (defaultMain)
 #endif
-import Gauge
 
 import Control.Teardown (newTeardown, runTeardown_)
 
 main :: IO ()
 main = defaultMain
   [ bgroup
-    "simple IO unit return"
-    [ bench "without teardown" (whnfIO $ return ())
-    , env
-      (newTeardown "benchmark" (return () :: IO ()))
-      (\unitTeardown ->
-        bench "with teardown" (whnfIO $ runTeardown_ unitTeardown)
-      )
-    ]
+      "simple IO unit return"
+      [ bench "without teardown" (whnfIO $ return ())
+      , env
+        (newTeardown "benchmark" (return () :: IO ()))
+        (\unitTeardown ->
+          bench "with teardown" (whnfIO $ runTeardown_ unitTeardown)
+        )
+      ]
   , env
-    (   sequence (replicate 1000 (newTeardown "benchmark" (return () :: IO ())))
-    >>= newTeardown "parent"
-    )
-    (\composedTeardown ->
-      bench "teardown list" (whnfIO $ runTeardown_ composedTeardown)
-    )
+      (sequence (replicate 1000 (newTeardown "benchmark" (return () :: IO ())))
+       >>= newTeardown "parent")
+      (\composedTeardown ->
+          bench "teardown list" (whnfIO $ runTeardown_ composedTeardown))
   ]
